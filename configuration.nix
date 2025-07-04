57e4a57f540228a2c75e8d4f6c50c115c6aeee90
Alessandro Re <ale@ale-re.net>
2025-07-04 23:53:45 +0200

    chore: test2

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,12 @@
   environment.systemPackages = [ pkgs.ncdu ];
 
   # Root has passwordless login
-  users.users.root.initialPassword = "";
+  users.users.root.initialPassword = "cicer1";
+
+  networking.firewall.enable = false;
+
+  services.sshd.enable = true;
+  services.openssh.settings.PermitRootLogin = "yes";
 
   system.stateVersion = "25.05";
 }
