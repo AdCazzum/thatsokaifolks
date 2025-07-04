3d678128eefc625540579a28cfc64389f659268b
Alessandro Re <ale@ale-re.net>
2025-07-04 23:20:24 +0200

    chore: test

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,9 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
+
+  environment.systemPackages = [ pkgs.ncdu ];
+
   # Root has passwordless login
   users.users.root.initialPassword = "";
+
   system.stateVersion = "25.05";
 }
