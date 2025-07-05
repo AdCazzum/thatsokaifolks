6947ea7dac2ac40a30a1e9da5d7f10bb4122f35c
Alessandro Re <ale@ale-re.net>
2025-07-05 14:10:03 +0200

    feat: re-enabled image

--- flake.nix
+++ flake.nix
@@ -84,6 +84,27 @@
           '';
         };
 
+        # This is always an x86_64-linux image
+        packages.default = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          modules = [
+            # you can include your own nixos configuration here, i.e.
+            ./configuration.nix
+          ];
+          format = "qcow";
+
+          # optional arguments:
+          # explicit nixpkgs and lib:
+          # pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          # lib = nixpkgs.legacyPackages.x86_64-linux.lib;
+          # additional arguments to pass to modules:
+          # specialArgs = { myExtraArg = "foobar"; };
+
+          # you can also define your own custom formats
+          # customFormats = { "myFormat" = <myFormatModule>; ... };
+          # format = "myFormat";
+        };
+
         # This fails, unsure why
         # packages.walrus = pkgs.callPackage ./walrus.nix { };
         # This fails with cargo workspace stuff, unsure why
@@ -112,26 +133,6 @@
 
         #packages.x86_64-linux.sui-testnet =
         # nixpkgs.legacyPackages.x86_64-linux.callPackage ./sui-testnet.nix { };
-
-        # packages.x86_64-linux.default = nixos-generators.nixosGenerate {
-        #   system = "x86_64-linux";
-        #   modules = [
-        #     # you can include your own nixos configuration here, i.e.
-        #     ./configuration.nix
-        #   ];
-        #   format = "qcow";
-
-        #   # optional arguments:
-        #   # explicit nixpkgs and lib:
-        #   # pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        #   # lib = nixpkgs.legacyPackages.x86_64-linux.lib;
-        #   # additional arguments to pass to modules:
-        #   # specialArgs = { myExtraArg = "foobar"; };
-
-        #   # you can also define your own custom formats
-        #   # customFormats = { "myFormat" = <myFormatModule>; ... };
-        #   # format = "myFormat";
-        # };
 
         nixosConfigurations.vm-preview = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
